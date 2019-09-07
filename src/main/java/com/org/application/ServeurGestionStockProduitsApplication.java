1a9e5d3c834f729575df7a756f64f35e189a7d8e
Al Aabqary Ayman <abqary.ayman@gmail.com>
2019-09-07 17:07:19 +0100

    firstCommitTest

---
+++ src/main/java/com/org/application/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.org.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+	}
+
+}
